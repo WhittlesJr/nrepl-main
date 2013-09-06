7473bff29c047122453ca9b6c5441c2a121db89f
Hugo Duncan <hugo@hugoduncan.org>
2013-09-06 12:18:08 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nrepl-main "0.1.0"
+(defproject nrepl-main "0.1.1-SNAPSHOT"
   :description "Provide a main for running an nrepl server."
   :url "https://github.com/pallet/nrepl-main"
   :license {:name "Eclipse Public License"
