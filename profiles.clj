df8b5092b551d297937c403971f0196104c0c499
Hugo Duncan <hugo@hugoduncan.org>
2013-10-10 13:36:35 -0400

    Add doc profile

--- profiles.clj
+++ profiles.clj
@@ -4,4 +4,12 @@
  :no-checkouts {:checkout-shares ^:replace []} ; disable checkouts
  :clojure-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
  :clojure-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
- :clojure-1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+ :clojure-1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+ :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
+       :codox {:writer codox-md.writer/write-docs
+               :output-dir "doc/0.1.0/api"
+               :src-dir-uri "https://github.com/pallet/pallet/blob/develop"
+               :src-linenum-anchor-prefix "L"}
+       :aliases {"marg" ["marg" "-d" "doc/0.1.0/annotated"]
+                 "codox" ["doc"]
+                 "doc" ["do" "codox," "marg"]}}}
